e5eee35f933f536e546a55be120599067cb00f0e
= <=>
2023-01-17 13:51:16 +0000

    New model

---
+++ backend/views/interior-colors/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use backend\models\InteriorColors;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var backend\models\InteriorColorsSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = Yii::t('app', 'Interior Colors');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="interior-colors-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Interior Colors'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'color',
+            'status',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, InteriorColors $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
